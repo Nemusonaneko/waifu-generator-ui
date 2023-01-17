544a3867d18b87d028a5f74802ff0bbc1a70d2c3
nemusona <nemusonaneko@protonmail.com>
2023-01-17 02:52:05 -0600

    increase rate limit

--- components/Waifu/index.tsx
+++ components/Waifu/index.tsx
@@ -45,7 +45,7 @@ export default function Waifu() {
       { prevBlob: waifuData?.url, values: values, random: false },
       {
         onSettled: () => {
-          setCountdown(10);
+          setCountdown(20);
         },
       }
     );
@@ -56,7 +56,7 @@ export default function Waifu() {
       { prevBlob: waifuData?.url, values: null, random: true },
       {
         onSettled: () => {
-          setCountdown(10);
+          setCountdown(20);
         },
       }
     );
