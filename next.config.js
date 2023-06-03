6629594b12b4f8f008e365e0f06b0ebcdfb6eb2e
nemusona <nemusonaneko@protonmail.com>
2023-06-02 23:07:30 -0500

    fix domain?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["https://waifus.nemusona.com/"]
+  }
 }
 
 module.exports = nextConfig
